3a7635a38149fb35249e51b3cd0333718599ff36
muboluo <muboluo@126.com>
2022-01-20 22:46:46 +0800

    Update SecondActivity.java

--- app/src/main/java/app/test/intent/annotate/SecondActivity.java
+++ app/src/main/java/app/test/intent/annotate/SecondActivity.java
@@ -9,11 +9,17 @@ public class SecondActivity extends AppCompatActivity {
     @InjectIntent("temp1")
     public String temp1;
 
+    @InjectIntent("")
+    public String temp2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         InjectViewUtil.injectIntent(this);
+
+        System.out.println(temp1);
+        System.out.println(temp2);
     }
 
 }
